fce122afe92c4f527d99acd84a14ca73fbb10696
MoamenEslam <83357555+MoamenEslam@users.noreply.github.com>
2023-12-13 18:32:21 +0200

    Add files via upload

---
+++ Lab3.m
@@ -0,0 +1,11 @@
+%power of 2 question
+x = input('Enter Number : ');  % Prompt the user to enter a number and store it in the variable x
+p = 1;  % Initialize the variable p(power) to 1
+R = 1;  % Initialize the variable R(result) to 1
+
+while p <= x  % Repeat the following block of code as long as p is less than or equal to x
+    R = 2 * R;  % Multiply the value of R by 2 and update the value of R
+    p = p + 1;  % Increment the value of p by 1
+end
+
+disp(R);  % Display the final value of R
